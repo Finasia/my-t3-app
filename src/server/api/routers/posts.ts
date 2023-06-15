f0703bb43f448952a8f4ffb53b9dd3826e1de931
fancier <fancier2093@gmail.com>
2023-06-15 20:24:53 +0800

    通过clerkClient获取用户信息(id和image), 并根据用户id查找出属于该用户的posts进行render（PostView Component）
    t3的trpc react-query的使用（useQuery、isLoading）

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,38 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {clerkClient} from "@clerk/nextjs";
+import type {User} from "@clerk/backend";
+import {TRPCError} from "@trpc/server";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100
+    })).map(filterUserForClient);
+    console.log('users:',users);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author || !author.username) throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Author for post not found" });
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username //emmm 之所以这里脱裤子放屁式的这么操作，是因为ts太蠢了，它识别不了我们上已经在author.username为空的时候抛出了异常，走到这里来的一定是不为空的，但是ts还是会报错，所以我们只能这么做了
+        }
+      }
+    })
   }),
 });
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  }
+}
