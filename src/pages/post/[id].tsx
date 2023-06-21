35b86cf626302150c798150bc89f7b264562ed4a
fancier <fancier2093@gmail.com>
2023-06-22 01:22:16 +0800

    doc: 补充注释

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -40,6 +40,24 @@ export const getStaticProps: GetStaticProps = async (context) => {
   };
 };
 
+//https://nextjs.org/docs/pages/api-reference/functions/get-static-paths#fallback-blocking
+//fallback: "blocking" 什么意思？
+//
+//The paths returned from getStaticPaths will be rendered to HTML at build time by getStaticProps.
+//
+//The paths that have not been generated at build time will not result in a 404 page.
+//  Instead, Next.js will SSR on the first request and return the generated HTML.
+//
+//When complete, the browser receives the HTML for the generated path.
+//  From the user’s perspective, it will transition from "the browser is requesting the page" to "the full page is loaded".
+//  There is no flash of loading/fallback state.
+//
+//At the same time, Next.js adds this path to the list of pre-rendered pages.
+//  Subsequent requests to the same path will serve the generated page, like other pages pre-rendered at build time.
+//
+//fallback: 'blocking' will not update generated pages by default. To update generated pages, use Incremental Static Regeneration in conjunction with fallback: 'blocking'.
+//
+//Good to know: fallback: 'blocking' is not supported when using output: 'export'.
 export const getStaticPaths = () => {
   return { paths: [], fallback: "blocking" };
 };
